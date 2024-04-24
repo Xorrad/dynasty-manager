473a3a3a38a4709f996c5c0b35d890f8dcd87e09
Xorrad <monsieurs.aymeric@gmail.com>
2024-04-24 21:15:39 +0200

    test: setup junit tests

---
+++ src/test/Tests.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests {
+
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+
+}
